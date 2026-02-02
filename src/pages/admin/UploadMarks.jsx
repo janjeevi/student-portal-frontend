f1cd8558852ecd266c3e4231fd7abf514cc32588
Jan Jimin <janjimin@gmail.com>
2026-02-02 12:06:12 +0530

    done

--- src/pages/admin/UploadMarks.jsx
+++ src/pages/admin/UploadMarks.jsx
@@ -57,7 +57,7 @@ const API = import.meta.env.VITE_API_URL
       }
     })
 
-    await fetch("http://localhost:3000/marks", {
+    await fetch(`${API}/marks`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(records)
